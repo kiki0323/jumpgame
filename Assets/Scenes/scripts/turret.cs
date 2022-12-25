f390246e5acc108dcabf08b50edf0118dc8f26fb
kiki0323 <ba40830@gmail.com>
2022-12-25 22:01:40 +0800

    最後

--- Assets/Scenes/scripts/turret.cs
+++ Assets/Scenes/scripts/turret.cs
@@ -11,11 +11,6 @@ public class turret : MonoBehaviour
         InvokeRepeating("Bulletshot", 0, 2f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void Bulletshot()
     {
         Instantiate(bullet, new Vector3(Random.Range(-3,3), 4, 0), Quaternion.identity);
